735312d8bcdbc55c7958a2930657c96ed5da7f12
thahira97 <thahirasheerin3@gmail.com>
2023-05-25 18:02:37 -0400

    More about routing

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlace = function () {
+  return <h1>NewPlace works!</h1>
+}
+
+export default NewPlace
---
+++ src/user/pages/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Users = function () {
+  return <h1>User works!</h1>;
+};
+
+export default Users;
