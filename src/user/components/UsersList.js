10ee7411c359f3826424c205552f9f017b880cc4
thahira97 <thahirasheerin3@gmail.com>
2023-05-25 19:07:50 -0400

    Add UserList component

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import UserItem from "./UserItem";
+import "./UsersList.css";
+
+const UsersList = function (props) {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h1>No Users Found</h1>
+      </div>
+    );
+  }
+  return (
+    <ul>
+      {props.items.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.place}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default UsersList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = function () {
-  return <h1>User works!</h1>;
+  const USERS = [
+    {
+      id: "u2",
+      name: "Thahira",
+      image:
+        "https://images.pexels.com/photos/5206279/pexels-photo-5206279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      place: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
