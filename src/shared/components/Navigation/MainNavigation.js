b96882d9c55f5b511e9fca8191ffe13145a62661
thahira97 <thahirasheerin3@gmail.com>
2023-05-27 18:20:11 -0400

    Use hooks to manage the sideDrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
@@ -7,15 +7,20 @@ import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+  const openDrawer = () => {
+    setDrawerIsOpen(true)
+  }
   return (
     <React.Fragment>
-      <SideDrawer>
+      { drawerIsOpen && <SideDrawer>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
-      </SideDrawer>
+      </SideDrawer>}
+  
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}> 
           <span />
           <span />
           <span />
