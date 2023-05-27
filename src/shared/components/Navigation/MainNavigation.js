ee9f6f9db31161b41047f085f7fd7661e634e141
thahira97 <thahirasheerin3@gmail.com>
2023-05-27 16:42:16 -0400

    Add MainNavigation comp

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import MainHeader from "./MainHeader";
 import "./MainNavigation.css";
 
-const MainNavigation = (props) => {};
+const MainNavigation = (props) => {
+  return <MainHeader >
+    <button className="main-navigation__menu-btn">
+    <span />
+    <span />
+    <span />
+    </button>
+    <h1 className="main-navigation__title">Your Places</h1>
+
+  </MainHeader>
+};
 
 export default MainNavigation
