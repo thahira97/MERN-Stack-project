fe06d36548041327768d1cd41bc1771c74072323
thahira97 <thahirasheerin3@gmail.com>
2023-05-28 13:38:53 -0400

    Add content to the modelOverLay

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,14 +1,30 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 
 import "./Modal.css";
 
 const ModalOverLay = (props) => {
   const content = (
-    <div className={`modal ${props.className}`}></div>
-  )
- return ReactDOM.createPortal(content, document.getElementById('modal-hook'))
-}
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
+};
 
 const Modal = (props) => {
   return;
