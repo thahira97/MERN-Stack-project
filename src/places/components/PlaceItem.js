9b904e51cda297ce23420b2f38c6933ea9ba7028
thahira97 <thahirasheerin3@gmail.com>
2023-05-27 23:15:43 -0400

    Add PlayItem component

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,5 +1,24 @@
 import React from "react";
+import Card from "../../shared/components/UIElements/Card";
 
-const PlaceItem = () => {};
+const PlaceItem = (props) => {
+  <li className="place-item">
+    <Card>
+      <div className="place-item__image">
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className="place-item__info">
+        <h2>{props.title}</h2>
+        <h3>{props.address}</h3>
+        <p>{props.description}</p>
+      </div>
+      <div className="place-item__actions">
+        <button>View On Map</button>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </Card>
+  </li>;
+};
 
 export default PlaceItem;
