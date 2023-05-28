3f43367226f7bd8d962e8de2f318c12966dabfc1
thahira97 <thahirasheerin3@gmail.com>
2023-05-28 00:09:41 -0400

    Filter according to the users

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
 
@@ -29,7 +30,9 @@ creator: 'u2'
 ;
 
 const UserPlace = function (props) {
-  return <PlaceList items={DUMMY_PLACES} />
+  const userId = useParams().userId
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId)
+  return <PlaceList items={loadedPlaces} />
 }
 
 export default UserPlace
