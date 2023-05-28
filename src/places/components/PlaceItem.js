30f32ab862790b9ab59f85036f3350be291357d2
thahira97 <thahirasheerin3@gmail.com>
2023-05-28 14:58:40 -0400

    Pass props to Modal from PlaceItem

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -13,23 +13,37 @@ const PlaceItem = (props) => {
 
   const closeMapHandler = () => setShowMap(false);
   return (
-    <li className="place-item">
-      <Card>
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button inverse>View On Map</Button>
-          <Button to={`/places/${props.id}`}>Edit</Button>
-          <Button danger>Delete</Button>
-        </div>
-      </Card>
-    </li>
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+        >
+          <div className="map-container">
+            
+          </div>
+      </Modal>
+      <li className="place-item">
+        <Card>
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse>View On Map</Button>
+            <Button to={`/places/${props.id}`}>Edit</Button>
+            <Button danger>Delete</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 };
 
