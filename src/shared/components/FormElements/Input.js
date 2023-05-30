1c3577fc15cb19ad484cba838d6bb2e85b8cc6b3
thahira97 <thahirasheerin3@gmail.com>
2023-05-30 13:46:56 -0400

    Add inputReducer function

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -2,25 +2,36 @@ import React, { useReducer } from "react";
 
 import "./Input.css";
 
-const inputReducer = () => {
-
-}
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: true 
+      };
+    default:
+      return state;
+  }
+};
 
 const Input = (props) => {
-  useReducer();
+  useReducer(inputReducer);
 
-  const changeHandler = event => {
-
-  }
+  const changeHandler = (event) => {};
 
   const element =
     props.element === "input" ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} onChange={changeHandler} />
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+      />
     ) : (
       <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} />
     );
 
-    
   return (
     <div className={`form-control`}>
       <label htmlFor={props.id}>{props.label}</label>
