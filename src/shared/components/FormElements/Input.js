1e6c959dc691572bc9e1e803a6e0e08fa068f6b8
thahira97 <thahirasheerin3@gmail.com>
2023-05-30 13:35:33 -0400

    Use useReducer for managing complex state

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useReducer } from "react";
+
 import "./Input.css";
 
+const inputReducer = () => {
+
+}
+
 const Input = (props) => {
+  useReducer();
+
+  const changeHandler = event => {
+
+  }
+
   const element =
     props.element === "input" ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input id={props.id} type={props.type} placeholder={props.placeholder} onChange={changeHandler} />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3}/>
+      <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} />
     );
+
+    
   return (
     <div className={`form-control`}>
       <label htmlFor={props.id}>{props.label}</label>
